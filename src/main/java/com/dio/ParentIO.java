9162f1730cc8ec6e08f1867dc2d559ce6f806950
Dxh7788 <anan1215>
2018-01-05 18:04:48 +0800

    add javas

---
+++ src/main/java/com/dio/ParentIO.java
@@ -0,0 +1,12 @@
+package com.dio;
+
+/**
+ * @author xh.d
+ * @date 2018/1/5 18:03
+ */
+
+/**
+ * root io
+ * */
+public class ParentIO {
+}
---
+++ src/test/java/com/dio/ParentIOTest.java
@@ -0,0 +1,8 @@
+package com.dio;
+
+/**
+ * @author xh.d
+ * @date 2018/1/5 18:04
+ */
+public class ParentIOTest {
+}
